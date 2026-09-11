40600941b6c83cb76a5d0e9364d4dc7a6c6d0006
Jutchicken <bumyjutch0827@gmail.com>
2026-09-11 18:45:07 +1200

    admin done

--- app/src/main/java/com/example/assessment/AdminMainPage.java
+++ app/src/main/java/com/example/assessment/AdminMainPage.java
@@ -1,24 +1,62 @@
 package com.example.assessment;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AdminMainPage extends AppCompatActivity {
 
+    CardView cvUserManagement, cvPropertyManagement, cvBookingManagement;
+    Button btnALogout;
+    ImageButton ibAdminProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_main_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cvUserManagement = findViewById(R.id.cv_userManagement);
+        cvPropertyManagement = findViewById(R.id.cv_propertyManagement);
+        cvBookingManagement = findViewById(R.id.cv_bookingManagement);
+        btnALogout = findViewById(R.id.btn_aLogout);
+        ibAdminProfile = findViewById(R.id.ib_adminProfile);
+
+        cvUserManagement.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(),
+                    "Feature for User management",
+                    Toast.LENGTH_SHORT).show();
         });
+
+        cvPropertyManagement.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(),
+                    "Feature for Property management",
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        cvBookingManagement.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(),
+                    "Feature for Booking management",
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        ibAdminProfile.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(),
+                    "Feature for Admin profile",
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        btnALogout.setOnClickListener(v -> {
+            finish();
+        });
+
     }
 }
